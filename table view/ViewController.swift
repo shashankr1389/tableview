801675da3cd326ac253f367d141ee9c5e35d0ac4
Your Name <you@example.com>
2016-09-15 12:49:50 +0530

    table view
    populating table view with each cell has distinct colour

--- table view/ViewController.swift
+++ table view/ViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+    let subject = ["ram","shyam","mohan","kapil","rohit","suman","radha","modi","rahul","shail","anuj","kk","rohan","rohit"]
+    
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+    return subject.count
+    
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    
+    {
+         let cell = tableView.dequeueReusableCellWithIdentifier("done", forIndexPath: indexPath)
+        cell.textLabel?.text = subject[indexPath.row]
+        cell.textLabel?.backgroundColor = UIColor.clearColor()
+        cell.contentView.backgroundColor = UIColor.randomcolor()
+        return (cell)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +46,22 @@ class ViewController: UIViewController {
 
 
 }
+extension UIColor {
+    
+   static func randomcolor() -> UIColor {
+        
+        let randomBlue = drand48()
+        let randomRed = drand48()
+        let randomGreen = drand48()
+        let randomAlpha = drand48()
+        
+        
+        let color = UIColor(red: CGFloat(randomRed), green: CGFloat(randomGreen), blue: CGFloat(randomBlue), alpha: CGFloat(randomAlpha))
+        return color
+        
+    }
+    
+    
+}
+
 
